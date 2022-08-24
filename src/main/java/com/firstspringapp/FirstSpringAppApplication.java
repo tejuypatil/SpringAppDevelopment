a42dbf532fac553eacf33e8dc624d89dc03b04f7
tejuypatil <tejuypatil@gmail.com>
2022-08-24 16:05:53 +0530

    Added Program to display "Hello from BridgeLabz" using STS

--- src/main/java/com/firstspringapp/FirstSpringAppApplication.java
+++ src/main/java/com/firstspringapp/FirstSpringAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstSpringAppApplication {
 
     public static void main(String[] args) {
-        System.out.println("HI TEJU");
+        System.out.println("Hello from BridgeLabz");
         SpringApplication.run(FirstSpringAppApplication.class, args);
     }
 
