14d8f7b5ca159bc196b5fa41b2cec76f514b1f7a
tejuypatil <tejuypatil@gmail.com>
2022-08-24 15:08:12 +0530

    Added Program to create first spring application

---
+++ src/main/java/com/firstspringapp/FirstSpringAppApplication.java
@@ -0,0 +1,14 @@
+package com.firstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("HI TEJU");
+        SpringApplication.run(FirstSpringAppApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/firstspringapp/controller/HelloWebController.java
@@ -0,0 +1,4 @@
+package com.firstspringapp.controller;
+
+public class HelloWebController {
+}
