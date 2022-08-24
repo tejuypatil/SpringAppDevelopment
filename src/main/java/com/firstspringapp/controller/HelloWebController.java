114afac255198259b6bba8d5ff61312745cf63e0
tejuypatil <tejuypatil@gmail.com>
2022-08-24 16:11:43 +0530

    Added Program to display "Hello from BridgeLabz" in a Web Page

--- src/main/java/com/firstspringapp/controller/HelloWebController.java
+++ src/main/java/com/firstspringapp/controller/HelloWebController.java
@@ -1,4 +1,18 @@
 package com.firstspringapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloWebController {
+    @GetMapping("/web")
+    public String hello(){
+        return "hello";
+    }
+    @GetMapping("/web/message")
+    public String message(Model model){
+        model.addAttribute("message","This is a custom message.Hello from Bridglabz");
+        return "message";
+    }
 }
